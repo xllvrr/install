35a73b45c9b4614cb239a25881a1fffa47f69e5d
xllvr <dtsa.tan@gmail.com>
2020-05-09 15:17:16 +0800

    Autoremove bashprofile

--- install.sh
+++ install.sh
@@ -49,12 +49,9 @@ sudo ln -fs .profile .zprofile
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/xllvrr/dotfiles.git $HOME/.dotfiles
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+mkdir -p .config-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
 config checkout
 config config --local status.showUntrackedFiles no
 
