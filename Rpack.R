b03825b66c15c569a3ffed452fded54c8764756d
xllvr <dtsa.tan@gmail.com>
2020-12-20 12:29:16 +0800

    R backup

--- Rpack.R
+++
@@ -1,5 +0,0 @@
-installedPreviously <- read.csv('Rpack.csv')
-baseR <- as.data.frame(installed.packages())
-toInstall <- setdiff(installedPreviously, baseR)
-
-install.packages(c(toInstall),"languageserver")
